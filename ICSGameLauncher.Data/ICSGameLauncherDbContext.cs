b97e6a6cefad1c2f8ded1e2406190f5ed2aa90a9
Souček Matěj (270256) <xsoucem00@vut.cz>
2026-03-04 07:54:41 +0000

    Merged PR 12: feat: added DbContext
    Related work items: #14

---
+++ ICSGameLauncher.Data/ICSGameLauncherDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ICSGameLauncher.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICSGameLauncher.Data;
+
+public class ICSGameLauncherDbContext : DbContext
+{
+    public ICSGameLauncherDbContext(DbContextOptions<ICSGameLauncherDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Library> Libraries => Set<Library>();
+    public DbSet<Title> Titles => Set<Title>();
+    public DbSet<Studio> Studios => Set<Studio>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<TitleLibrary> TitleLibraries => Set<TitleLibrary>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Title>(entity =>
+        {
+            entity.HasMany(t => t.Libraries)
+                .WithMany(l => l.Titles)
+                .UsingEntity<TitleLibrary>();
+        });
+    }
+}
