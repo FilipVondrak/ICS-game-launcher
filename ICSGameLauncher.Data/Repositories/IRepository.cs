18e0503a46d64c2577607c53b05912c49477adc3
matejsoucek <matej.soucek1@gmail.com>
2026-03-27 10:32:26 +0100

    feat: add generic base repository

---
+++ ICSGameLauncher.Data/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ICSGameLauncher.Data.Repositories;
+
+public interface IRepository<TEntity>
+    where TEntity : class
+{
+    Task<List<TEntity>> GetAllAsync();
+    Task InsertAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
---
+++ ICSGameLauncher.Data/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICSGameLauncher.Data.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity>
+    where TEntity : class
+{
+    protected ICSGameLauncherDbContext DbContext { get; }
+    protected DbSet<TEntity> DbSet { get; }
+
+    public Repository(ICSGameLauncherDbContext dbContext)
+    {
+        DbContext = dbContext;
+        DbSet = dbContext.Set<TEntity>();
+    }
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await DbSet.ToListAsync();
+    }
+
+    public async Task InsertAsync(TEntity entity)
+    {
+        await DbSet.AddAsync(entity);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        DbSet.Update(entity);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(TEntity entity)
+    {
+        DbSet.Remove(entity);
+        await DbContext.SaveChangesAsync();
+    }
+}
