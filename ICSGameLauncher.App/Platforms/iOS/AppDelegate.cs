43d164512497b78734cfa60c43eedd26e8f2a45e
Bondar Oleksandr (272028) <xbondao00@vutbr.cz>
2026-05-06 13:23:21 +0000

    Merged PR 42: build: remove ios and maccatalyst
    build: remove ios and maccatalyst
    
    our app will not support macos and ios

--- ICSGameLauncher.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ICSGameLauncher.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
